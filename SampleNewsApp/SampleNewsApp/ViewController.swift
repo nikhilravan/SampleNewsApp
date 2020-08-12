5a630ca6620d71db2ed3c83ca8b5f038e3e52fcf
Nikhil Ravan <nikhilr@nousinfo.com>
2020-08-12 21:00:13 +0530

    Project created

---
+++ SampleNewsApp/SampleNewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleNewsApp
+//
+//  Created by Nikhil Ravan on 12/08/20.
+//  Copyright © 2020 Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
